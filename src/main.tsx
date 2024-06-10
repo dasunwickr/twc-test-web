8f165e99f1a64c70d055b786bd0c35f1f8ac8f92
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-06-10 15:36:43 +0530

    Initial FE packages install and Folder Structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import WelcomePage from "./pages";
+import LoginPage from "./pages/login";
+import NewContactPage from "./pages/contacts/new";
+import ContactsPage from "./pages/contacts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <WelcomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/contacts/new",
+    element: <NewContactPage />,
+  },
+  {
+    path: "/contacts",
+    element: <ContactsPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/contacts/new.tsx
@@ -0,0 +1,5 @@
+const NewContactPage = () => {
+  return <div>New Contacts Page</div>;
+};
+
+export default NewContactPage;
